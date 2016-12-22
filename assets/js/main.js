f20dcbef62035f5c84aeb82016d8d2c40e72ed86
Pavel Panaitov <pavel.panaitov@gmail.com>
2016-12-22 12:49:10 +0200

    delete comments in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,13 +15,13 @@
 			'nextArrow': '.slider-arrow--next'
 		}); // end slick
 
-		/*$('.intro-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+		$('.intro-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
 			$('.intro-slider .intro-slider__item').addClass('slide-hover');
 		}); //end beforeChange 
 	
 		$('.intro-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
 			$('.intro-slider .intro-slider__item').removeClass('slide-hover');
-		}); //end afterChange */
+		}); //end afterChange 
 		
 	}); // end ready
 
